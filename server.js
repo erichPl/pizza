a008d6f240bffced6f7cb233d124e9c718263032
erichPl <77007910+erichPl@users.noreply.github.com>
2026-02-27 12:04:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
-app.use(express.json());
+//app.use(express.json());
+
+// Erhöhe das Limit auf z.B. 10MB (statt der standardmäßigen 100KB)
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use(express.static('public'));
 
 // Ein extrem schneller Health-Check Endpunkt
@@ -90,4 +95,5 @@ app.post('/api/data', async (req, res) => {
 // START
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
+
 
