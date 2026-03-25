7caf52d2c892e68307500a69bf305fe67b3037de
erichPl <77007910+erichPl@users.noreply.github.com>
2026-03-25 17:34:03 +0100

    Update and rename serverBest.js to server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static(__dirname));
-
+//app.use(express.static(__dirname));
+app.use(express.static('public'));
 // --- BITTE NUR HIER DEINE URL EINTRAGEN ---
 const mongoURI =  process.env.MONGO_URI;
 
